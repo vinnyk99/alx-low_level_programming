4eae2ccbec892e9fb928dd4744b7cdcc12423dda
VINCENT KOECH <pngetichkoech6@gmail.com>
2023-03-16 13:53:17 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#inlude < stdio.h >
+/**
+ * main - a program that prints a line
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
